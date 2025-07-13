6a35c716203d721bac0cafb8d0ea3748b8dcb718
Soccertanker <e.smith.py@gmail.com>
2025-07-13 08:47:29 -0700

    working cactus farming
    plants and sorts cacti. Uses bubble sort to sort all rows then sort all columns

--- cactus_sort.py
+++ cactus_sort.py
@@ -1,56 +1,81 @@
-cols = 6
 rows = 6
+cols = 6
 
-def grow_cactus_row():
-	for j in range(cols):
-		till()
-		plant(Entities.Cactus)
-		move(East)
-		
-		
-def grow_all_cactuses():
+def plant_cacti():	
 	clear()
-	for i in range(rows):
-		grow_cactus_row()
+	for i in range(6):
+		for j in range(6):
+			till()
+			plant(Entities.Cactus)
+			move(East)
 		move(North)
 		
-		
-def move_drone_to_x_pos(x_target):
-	# Assume drone is on first row
+def move_to_x_pos(x_target):
 	x_curr = get_pos_x()
 	moves_needed = x_target - x_curr
 	if moves_needed > 0:
 		dir = East
 	else:
 		dir = West
-	
 	for i in range(abs(moves_needed)):
 		move(dir)
 
+def move_to_y_pos(y_target):
+	y_curr = get_pos_y()
+	moves_needed = y_target - y_curr
+	if moves_needed > 0:
+		dir = North
+	else:
+		dir = South
+	for i in range(abs(moves_needed)):
+		move(dir)
 
+def go_to_origin():
+	move_to_x_pos(0)
+	move_to_y_pos(0)
 
 def sort_row():
-	row_nums = list()
-	for i in range(cols):
-		row_nums.append(measure())
-		move(East)
-	
+	# Assume drone is at the first column
 	for i in range(cols):
 		swapped = False
 		for j in range(0, cols - i - 1):
-			move_drone_to_x_pos(j)
-			if row_nums[j] > row_nums[j + 1]:
+			move_to_x_pos(j)
+			if measure() > measure(East):
 				swap(East)
-				row_nums[j], row_nums[j + 1] = row_nums[j + 1], row_nums[j]
 				swapped = True
 		if not swapped:
 			break
 
+def sort_all_rows():
+	move_to_x_pos(0)
+	for i in range(rows):
+		move_to_y_pos(i)
+		sort_row()
+		
+def sort_col():
+	# Assume drone is at the first row
+	for i in range(rows):
+		swapped = False
+		for j in range(0, rows - i - 1):
+			move_to_y_pos(j)
+			if measure() > measure(North):
+				swap(North)
+				swapped = True
+		if not swapped:
+			break
+			
+def sort_all_cols():
+	move_to_y_pos(0)
+	for i in range(cols):
+		move_to_x_pos(i)
+		sort_col()
+		
+def sort_cacti():
+	sort_all_rows()
+	sort_all_cols()
 
-clear()
-
-for y in range(cols):
-	grow_cactus_row()
-	move_drone_to_x_pos(0)
-	sort_row()
-	move(North)
+while True:
+	plant_cacti()		
+	go_to_origin()
+	sort_cacti()
+	harvest()

8f34ff53d505c967daee8c53e7aa17fff8dd2fbe
Soccertanker <e.smith.py@gmail.com>
2025-07-26 01:35:18 -0700

    Update scalable_minimum_thresholds.py
    add pumpkin and cactus farming

--- scalable_minimum_thresholds.py
+++ scalable_minimum_thresholds.py
@@ -1,9 +1,9 @@
 world_size = get_world_size()
 
-# Currently only supports farming for Hay, Wood, and Carrots. Need to port other Item farming here.
-
 min_num_items = 10000
 water_threshold = 0.75
+mode_interval = 20
+pumpkin_fertilizer_needed = 200
 
 def prep_carrot():
 	till_all(Entities.Carrot)
@@ -34,8 +34,6 @@ def run_hay():
 	
 def prep_wood():
 	clear()
-	pass
-
 
 def run_wood():
 	global world_size
@@ -54,16 +52,133 @@ def run_wood():
 			move(North)
 		move(East)
 
-def till_all(plant_type):
+def prep_pumpkin():
+	till_all(None)
+
+def plant_all(plant_type):
+	global world_size
+	
+	for col in range(world_size):
+		for row in range(world_size):
+			plant(plant_type)
+			move(North)
+		move(East)
+
+def till_all(plant_type, water=True):
 	global world_size
 	
 	clear()
 	for col in range(world_size):
 		for row in range(world_size):
 			till()
-			plant(plant_type)
+			if water:
+				while get_water() < water_threshold:
+					use_item(Items.Water)
+			if plant_type:
+				plant(plant_type)
 			move(North)
 		move(East)
+		
+def fertilize_empties(plant_type):
+	global world_size
+	
+	for col in range(world_size):
+		for row in range(world_size):
+			while not can_harvest():
+				plant(plant_type)
+				use_item(Items.Fertilizer)
+			move(North)
+		move(East)
+
+def run_pumpkin():
+	global world_size
+	
+	plant_all(Entities.Pumpkin)
+	fertilize_empties(Entities.Pumpkin)
+	harvest()
+
+def move_to_x_pos(x_target):
+	x_curr = get_pos_x()
+	moves_needed = x_target - x_curr
+	if moves_needed > 0:
+		dir = East
+	else:
+		dir = West
+	for i in range(abs(moves_needed)):
+		if not move(dir):
+			hat_flip()
+
+def move_to_y_pos(y_target):
+	y_curr = get_pos_y()
+	moves_needed = y_target - y_curr
+	if moves_needed > 0:
+		dir = North
+	else:
+		dir = South
+	for i in range(abs(moves_needed)):
+		if not move(dir):
+			hat_flip()
+
+def go_to_origin():
+	move_to_x_pos(0)
+	move_to_y_pos(0)
+
+def sort_row():
+	global world_size
+	
+	# Assume drone is at the first column
+	for i in range(world_size):
+		swapped = False
+		for j in range(0, world_size - i - 1):
+			move_to_x_pos(j)
+			if measure() > measure(East):
+				swap(East)
+				swapped = True
+		if not swapped:
+			break
+
+def sort_all_rows():
+	global world_size
+	
+	move_to_x_pos(0)
+	for row in range(world_size):
+		move_to_y_pos(row)
+		sort_row()
+		
+def sort_col():
+	global world_size
+	
+	# Assume drone is at the first row
+	for i in range(world_size):
+		swapped = False
+		for j in range(0, world_size - i - 1):
+			move_to_y_pos(j)
+			if measure() > measure(North):
+				swap(North)
+				swapped = True
+		if not swapped:
+			break
+			
+def sort_all_cols():
+	global world_size
+
+	move_to_y_pos(0)
+	for col in range(world_size):
+		move_to_x_pos(col)
+		sort_col()
+		
+def sort_cacti():
+	sort_all_rows()
+	sort_all_cols()
+
+def prep_cactus():
+	till_all(None, False)
+
+def run_cactus():
+	go_to_origin()
+	plant_all(Entities.Cactus)
+	sort_cacti()
+	harvest()
 
 def get_mode():
 	global min_num_items
@@ -74,6 +189,10 @@ def get_mode():
 		return Items.Wood
 	elif num_items(Items.Carrot) < min_num_items:
 		return Items.Carrot
+	elif num_items(Items.Pumpkin) < min_num_items and num_items(Items.Fertilizer) > pumpkin_fertilizer_needed:
+		return Items.Pumpkin
+	elif num_items(Items.Cactus) < min_num_items:
+		return Items.Cactus
 	else:
 		min_num_items *= 2
 		return get_mode()
@@ -85,6 +204,10 @@ def prep_mode(mode):
 		prep_wood()
 	elif mode == Items.Carrot:
 		prep_carrot()
+	elif mode == Items.Pumpkin:
+		prep_pumpkin()
+	elif mode == Items.Cactus:
+		prep_cactus()
 
 def run_mode(mode):
 	if mode == Items.Hay:
@@ -93,8 +216,11 @@ def run_mode(mode):
 		run_wood()
 	elif mode == Items.Carrot:
 		run_carrot()
+	elif mode == Items.Pumpkin:
+		run_pumpkin()
+	elif mode == Items.Cactus:
+		run_cactus()
 	
-mode_interval = 20
 mode_cnt = 0
 curr_mode = None
 while True:

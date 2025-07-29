18d918001290b432c858a9a72b66c236dbfc9faa
Matthew Smith <matthewksmith12@gmail.com>
2025-07-28 20:35:24 -0700

    Throwing a lot of untested code together'

--- sunflower_leaderboard.py
+++ sunflower_leaderboard.py
@@ -60,6 +60,76 @@ def go_to_origin():
 	move_to_x_pos(0)
 	move_to_y_pos(0)
 
+def get_distance(x_curr, y_curr, x_target, y_target):
+	if x_target > x_curr:
+		east_moves = x_target - x_curr
+	else:
+		east_moves = x_target + (world_size - x_curr)
+	west_moves = world_size - east_moves
+	
+	x_moves = min(west_moves, east_moves)
+	
+	if y_target > y_curr:
+		north_moves = y_target - y_curr
+	else:
+		north_moves = y_target + (world_size - y_curr)
+	south_moves = world_size - north_moves
+
+	y_moves = min(north_moves, south_moves)
+	
+	return y_moves + x_moves
+
+def check_sunflower_ready(curr_ticks, sunflower_ticks, sunflower_water, distance_away, fertilizer_count):
+	ticks_elapsed_since_planted = curr_ticks - sunflower_ticks
+	# Takes around 140k ticks for sunflower to grow. Water at level 1 increases growth by 5x
+	SUNFLOWER_GROWTH_TIME = 100000.0
+	sunflower_time_to_grow = SUNFLOWER_GROWTH_TIME
+	if sunflower_water != 0:
+		sunflower_time_to_grow = SUNFLOWER_GROWTH_TIME / (5 * sunflower_water)
+	# Fertilizer reduces growth time by 25k ticks
+	sunflower_time_to_grow_with_fert = sunflower_time_to_grow - 25000 * fertilizer_count
+
+	ready_without_fertilizer =  ticks_elapsed_since_planted + 100 * distance_away > sunflower_time_to_grow
+	ready_with_fertilizer = ticks_elapsed_since_planted + 100 * distance_away > sunflower_time_to_grow_with_fert
+
+	return ready_without_fertilizer, ready_with_fertilizer
+
+# Get closest sunflower that is harvestable by the time we get there
+def get_closest_sunflower(x_curr, y_curr, sunflowers):
+	min_distance = 100
+	min_coordinate = (-1, -1)
+	backup_coordinate = (-1, -1)
+
+	# Each fertilizer removes about 25k ticks from grown
+	fertilizer_count = num_items(Items.Fertilizer)
+
+	for sunflower_pos in sunflowers:
+		fertilizer_required = False
+		sunflower_ticks = sunflowers[sunflower_pos]["ticks"]
+		sunflower_water = sunflowers[sunflower_pos]["water"]
+		distance = get_distance(x_curr, y_curr, sunflower_pos[0], sunflower_pos[1])
+
+		curr_ticks = get_tick_count()
+		
+		if distance < min_distance:
+			sunflower_ready, sunflower_ready_with_fertilizer = check_sunflower_ready(curr_ticks, sunflower_ticks, sunflower_water, distance, fertilizer_count)
+			if not (sunflower_ready or sunflower_ready_with_fertilizer):
+				# If we don't have a min yet, set a backup if we never get a min_coordinate
+				if min_coordinate == (-1, -1):
+					backup_coordinate = sunflower_pos
+				else:
+					continue
+			else:
+				requires_fertilizer = not sunflower_ready
+				min_distance = distance
+				min_coordinate = sunflower_pos
+				ret_data = min_coordinate, requires_fertilizer
+	
+	if min_coordinate == (-1, -1):
+		ret_data = backup_coordinate, False
+
+	return ret_data
+
 def run_sunflower():
 	global count 
 	global petal_min
@@ -79,9 +149,14 @@ def run_sunflower():
 			
 			if pedal_count:
 				if pedal_count not in sf_size_tracker:
-					sf_size_tracker[pedal_count] = []
+					sf_size_tracker[pedal_count] = {}
 
-				sf_size_tracker[pedal_count].append((x, y))
+				curr_ticks = get_tick_count()
+				curr_water = get_water()
+				sf_size_tracker[pedal_count][(x, y)] = {
+					"ticks": curr_ticks,
+					"water": curr_water
+				}
 			move(North)
 		move(East)
 	
@@ -92,23 +167,30 @@ def run_sunflower():
 		i = max_pedals - j
 		if i not in sf_size_tracker:
 			continue
-		targets_list = sf_size_tracker[i]
+		targets_dict = sf_size_tracker[i]
 		
-		for target in targets_list:
+		while len(targets_dict) > 0:
 			# We no longer get the power boost when less than 10 sunflowers remain
 			if harvest_counter < 10:
 				return False
 			
-			move_to_x_pos(target[0])
-			move_to_y_pos(target[1])
-			while not can_harvest() and num_items(Items.Fertilizer) > 1:
-				use_item(Items.Fertilizer)
-			if not can_harvest():
-				count += 1
+			x_curr = get_pos_x()
+			y_curr = get_pos_y()
+			(target_x, target_y), requires_fertilizer = get_closest_sunflower(x_curr, y_curr, targets_dict)
+
+			move_to_x_pos(target_x)
+			move_to_y_pos(target_y)
+
+			if requires_fertilizer:
+				while not can_harvest() and num_items(Items.Fertilizer) > 1:
+					use_item(Items.Fertilizer)
+
 			harvest() # sometimes we harvest a sunflower that is not yet fully grown. TODO track these cases?
 			harvest_counter -= 1
 			if num_items(Items.Power) >= 100000:
 				return True
+
+			targets_dict.pop((target_x, target_y))
 	
 	return False
 		

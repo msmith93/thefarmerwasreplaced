676f25cc3df58d1b984cc4b891b69948634ee76e
Matthew Smith <matthewksmith12@gmail.com>
2025-08-30 13:32:46 -0700

    Add tail tracking

--- dino_leaderboard.py
+++ dino_leaderboard.py
@@ -7,10 +7,27 @@ offlimit_columns_stage1 = {
 }
 offlimit_columns_stage2 = {
 }
+
+grid_data = [
+		[1, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+    
+
+
 clear()
 change_hat(Hats.Dinosaur_Hat)
 apple_pos = measure()
-apple_pos_queue = []
+tail_positions = [(0,0)]
+tail_positions_set = set(tail_positions)
 squares_occupied = 1
 game_complete = False
 aggressive_stage = True
@@ -19,18 +36,33 @@ aggressive_stage = True
 def measure_apple():
     global apple_pos
     global squares_occupied
-    global apple_pos_queue
     apple_pos = measure()
-    apple_pos_queue += [apple_pos]
     squares_occupied += 1
     
 def move_and_check_apple(direction):
     global apple_pos
+    global tail_positions
+	global tail_positions_set
+	global grid_data
+    
+    curr_pos = (get_pos_x(), get_pos_y())
 
     if not move(direction):
         return False
-    if (get_pos_x(), get_pos_y()) == apple_pos:
+       
+    curr_pos_x = get_pos_x()
+    curr_pos_y = get_pos_y()
+    curr_pos = (curr_pos_x, curr_pos_y)
+    tail_positions.insert(0, curr_pos)
+    tail_positions_set.add(curr_pos)
+    grid_data[curr_pos[0]][curr_pos[1]] = 1
+        
+    if curr_pos == apple_pos:
         measure_apple()
+    else:
+        popped_item = tail_positions.pop()
+        grid_data[popped_item[0]][popped_item[1]] = 0
+        tail_positions_set.remove(popped_item)
 
     return True
 
@@ -48,6 +80,8 @@ def move_to_col(target_x_pos, do_measure=True):
         if do_measure:
             this_check = move_and_check_apple 
         if not this_check(direction):
+            if do_measure:
+                pass
             return False
     
     return True
@@ -70,26 +104,77 @@ def move_to_row(target_y_pos, do_measure=True):
     
     return True
 
-def transition_to_stage_2():
+def transition_to_stage_2(panic_mode=False):
     global offlimit_columns_stage1
     global world_size
     global world_size_minus_one
+    global offlimit_columns_stage2
     
-    move_to_col(world_size_minus_one)
-    move_to_row(0)
+    if panic_mode:
+        move_and_check_apple(East)
+        curr_x = get_pos_x()
+        if not curr_x % 2:
+            move_and_check_apple(East)
+        while curr_x < world_size_minus_one:
+            if curr_x in offlimit_columns_stage1:
+                target_y_pos = offlimit_columns_stage1[curr_x] + 1
+                move_to_row(target_y_pos)
+                offlimit_columns_stage2[curr_x] = target_y_pos
+                offlimit_columns_stage2[curr_x + 1] = target_y_pos
+            else:
+                move_to_row(5)
+                offlimit_columns_stage2[curr_x] = 5
+                offlimit_columns_stage2[curr_x + 1] = 5
+            
+            move_and_check_apple(East)
+            move_to_row(world_size_minus_one)
+            move_and_check_apple(East)
+
+            curr_x = get_pos_x()
+        
+        move_to_row(0)
+    else:
+        move_to_col(world_size_minus_one)
+        move_to_row(0)
     
     offlimit_columns_stage1 = {
     }
+
     
     return False
 
-def transition_to_stage_1():
+def transition_to_stage_1(panic_mode=False):
     global offlimit_columns_stage2
+    global offlimit_columns_stage1
     global world_size
     global world_size_minus_one
-    
-    move_to_col(0)
-    move_to_row(world_size_minus_one)
+
+    if panic_mode:
+        move_and_check_apple(West)
+        curr_x = get_pos_x()
+        if curr_x % 2:
+            move_and_check_apple(West)
+        while curr_x > 0:
+            if curr_x in offlimit_columns_stage2:
+                target_y_pos = offlimit_columns_stage2[curr_x] - 1
+                move_to_row(target_y_pos)
+                offlimit_columns_stage1[curr_x] = target_y_pos
+                offlimit_columns_stage1[curr_x - 1] = target_y_pos
+            else:
+                move_to_row(4)
+                offlimit_columns_stage1[curr_x] = 4
+                offlimit_columns_stage1[curr_x - 1] = 4
+            
+            move_and_check_apple(West)
+            move_to_row(0)
+            move_and_check_apple(West)
+
+            curr_x = get_pos_x()
+        
+        move_to_row(world_size_minus_one)
+    else:
+        move_to_col(0)
+        move_to_row(world_size_minus_one)
     
     offlimit_columns_stage2 = {
     }
@@ -102,8 +187,18 @@ def stage_1_apple_collect():
     global offlimit_columns_stage1
     global offlimit_columns_stage2
     global world_size_minus_one
-    
+    global squares_occupied
+
     apple_pos_x, apple_pos_y = apple_pos
+
+    buffer_needed = 0
+    if squares_occupied > 35:
+        buffer_needed = squares_occupied - 35
+    
+    apple_buffer = (world_size_minus_one - apple_pos_y) * 2
+    if buffer_needed > apple_buffer:
+        return transition_to_stage_2(True)
+    
     if apple_pos_y == 0 or apple_pos_x in edge_positions or (apple_pos_x in offlimit_columns_stage1 and apple_pos_y <= offlimit_columns_stage1[apple_pos_x]):
         return transition_to_stage_2()
     else:
@@ -112,7 +207,7 @@ def stage_1_apple_collect():
         if not apple_x_odd:
             target_x_pos = apple_pos_x - 1
         
-        if target_x_pos <= get_pos_x():
+        if target_x_pos <= get_pos_x() or (apple_pos_y < 3 and apple_pos_x > 7):
             return transition_to_stage_2()
         else:
             move_to_col(target_x_pos)
@@ -130,8 +225,18 @@ def stage_2_apple_collect():
     global offlimit_columns_stage1
     global offlimit_columns_stage2
     global world_size_minus_one
+    global squares_occupied
     
     apple_pos_x, apple_pos_y = apple_pos
+
+    buffer_needed = 0
+    if squares_occupied > 35:
+        buffer_needed = squares_occupied - 35
+    
+    apple_buffer = apple_pos_y * 2
+    if buffer_needed > apple_buffer:
+        return transition_to_stage_1(True)
+
     if apple_pos_y == world_size_minus_one or apple_pos_x in edge_positions or (apple_pos_x in offlimit_columns_stage2 and apple_pos_y >= offlimit_columns_stage2[apple_pos_x]):
         return transition_to_stage_1()
     else:
@@ -140,7 +245,7 @@ def stage_2_apple_collect():
         if apple_x_odd:
             target_x_pos = apple_pos_x + 1
         
-        if target_x_pos >= get_pos_x():
+        if target_x_pos >= get_pos_x()  or (apple_pos_y > 7 and apple_pos_x < 3):
             return transition_to_stage_1()
         else:
             move_to_col(target_x_pos)
@@ -203,7 +308,7 @@ while aggressive_stage:
     while stage_2_apple_collect() and get_pos_x() > 0:
         pass
     
-    if squares_occupied > (world_size_minus_one) * 4 + 2:
+    if squares_occupied > (world_size_minus_one) * 4 + 1:
         break
 
 transition_to_route()
@@ -220,11 +325,9 @@ while True:
             break
 
     game_complete = game_complete or not move_to_row(0, False)
-    game_complete = game_complete or not move_to_col(0,)
+    game_complete = game_complete or not move_to_col(0,False)
     
     if game_complete:
         break
         
 change_hat(Hats.Straw_Hat)
-
-

7cc8752361bf6218c93f646288707320740107a6
Matthew Smith <matthewksmith12@gmail.com>
2025-10-17 21:10:15 -0700

    polyculture carrots

---
+++ multidrone/carrot.py
@@ -0,0 +1,60 @@
+clear()
+
+hats = [Hats.Green_Hat, Hats.Purple_Hat]
+world_size = get_world_size()
+
+def init_action():
+	global world_size
+	
+	for i in range(world_size):
+		if ((get_pos_x() + get_pos_y()) % 2):
+			till()
+			plant(Entities.Carrot)
+		else:
+			plant(Entities.Bush)
+			
+		move(North)
+
+drones = []
+for i in range(world_size - 1):
+	drones.append(spawn_drone(init_action))
+	move(East)
+
+init_action()
+move(East)
+
+def run_drone():
+	global hats
+	
+	while True:
+		if can_harvest():
+			harvest()
+			if ((get_pos_x() + get_pos_y()) % 2):
+				plant(Entities.Carrot)
+				use_item(Items.Water)
+				companion, comp_pos = get_companion()
+		
+				while companion != Entities.Bush or ((comp_pos[0] + comp_pos[1]) % 2):
+					harvest()
+					plant(Entities.Carrot)
+					companion, comp_pos = get_companion()
+			else:
+				plant(Entities.Bush)
+		move(North)
+
+def run_harvests():
+	global world_size
+	
+	harvest_drones = []
+	for i in range(world_size):
+		if len(harvest_drones) == 31:
+			return
+		while num_drones() >= max_drones():
+			pass
+				
+		harvest_drones.append(spawn_drone(run_drone))
+		move(East)
+
+run_harvests()
+run_drone()
+	
